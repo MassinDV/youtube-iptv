fe90828579362d08bc5d44add6bab0a07115d431
Massin <40926996+MassinDV@users.noreply.github.com>
2023-11-06 18:13:00 +0100

    Update ch12.py
    sunna

--- scripts/ch12.py
+++ scripts/ch12.py
@@ -18,7 +18,7 @@ def grab(url):
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://www.youtube.com/channel/UCROKYPep-UuODNwyipe6JMw/live')
+            print('https://raw.githubusercontent.com/MassinDV/youtube-iptv/main/assets/offlintv.m3u8')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
